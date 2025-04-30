02443deb78838cad0333f63358dad5013ee7853c
Kartik Saxena <karrtiksaxena@gmail.com>
2025-05-01 00:47:53 +0530

    new

--- app.py
+++ app.py
@@ -70,4 +70,4 @@ def upload_and_edit():
     return send_file(output_path, as_attachment=True, download_name=output_filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0', port=5000)
